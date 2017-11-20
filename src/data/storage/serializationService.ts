2708e8d4fb9cef108edfc60f2cce5d327317eb6c
Yatrik Solanki <yatriksolanki@gmail.com>
2017-11-20 15:33:57 -0500

    Add helper function to get Blob type from dataUrl string

--- src/data/storage/serializationService.ts
+++ src/data/storage/serializationService.ts
@@ -159,3 +159,26 @@ function getBase64FromDataUrl(safeDataUrl): string {
     safeDataUrl.changingThisBreaksApplicationSecurity : safeDataUrl;
   return dataUrlString.substring(dataUrlString.indexOf(',') + 1);
 }
+
+function getBlobFromDataUrl(safeDataUrl): any {
+  // From https://stackoverflow.com/a/12300351
+  // convert base64 to raw binary data held in a string
+
+  const dataUrlString: string = safeDataUrl.changingThisBreaksApplicationSecurity ?
+    safeDataUrl.changingThisBreaksApplicationSecurity : safeDataUrl;
+
+  var byteString = atob(dataUrlString.split(',')[1]);
+  // separate out the mime component
+  var mimeString = dataUrlString.split(',')[0].split(':')[1].split(';')[0]
+  // write the bytes of the string to an ArrayBuffer
+  var ab = new ArrayBuffer(byteString.length);
+  // create a view into the buffer
+  var ia = new Uint8Array(ab);
+  // set the bytes of the buffer to the correct values
+  for (var i = 0; i < byteString.length; i++) {
+    ia[i] = byteString.charCodeAt(i);
+  }
+  // write the ArrayBuffer to a blob, and you're done
+  var blob = new Blob([ab], {type: mimeString});
+  return blob;
+}
