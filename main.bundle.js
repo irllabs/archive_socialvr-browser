ff854cc69af541b1d1002fb41e3bce75e825b8b8
Luke Anderson <luke.kansas@gmail.com>
2018-01-22 15:22:53 -0600

    publish: close modals on app load
    generated from commit 4a9de8d628dd6a889e5aa9f70b7a386ccd9c65c2

--- main.bundle.js
+++ main.bundle.js
@@ -41122,17 +41122,26 @@ var __decorate = this && this.__decorate || function (decorators, target, key, d
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = this && this.__metadata || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = __webpack_require__(2);
 var core_2 = __webpack_require__(2);
+var router_1 = __webpack_require__(17);
 var Ui = /** @class */function () {
-    function Ui() {}
+    function Ui(router) {
+        this.router = router;
+    }
+    Ui.prototype.ngOnInit = function () {
+        this.router.navigate([{ outlets: { modal: null } }]);
+    };
     Ui = __decorate([core_1.Component({
         selector: 'app',
         styles: [__webpack_require__(1353)],
         template: __webpack_require__(1354),
         encapsulation: core_2.ViewEncapsulation.None
-    })], Ui);
+    }), __metadata("design:paramtypes", [router_1.Router])], Ui);
     return Ui;
 }();
 exports.Ui = Ui;
