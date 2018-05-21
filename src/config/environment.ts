f746cd8b043f8acd58ef1a932b48c9cd01bb7445
Emil Temirov <emil.temirov@litslink.com>
2018-05-21 19:43:26 +0300

    Firebase: Step 2 - Move #122:

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,14 +1,25 @@
+const build = require('build');
+
+const firebase = build === 'PROD' ? {
+  apiKey: 'AIzaSyC5Q5Ie9To_fE2Yk8jOq1BCjIlV-9SEqQM',
+  authDomain: 'social-vr-161302.firebaseapp.com',
+  databaseURL: 'https://social-vr-161302.firebaseio.com',
+  projectId: 'social-vr-161302',
+  storageBucket: 'social-vr-161302.appspot.com',
+  messagingSenderId: '613942124685',
+} : {
+  apiKey: 'AIzaSyAYEF9C8sje4GzAYoKJVO9dhXNSp_k31mw',
+  authDomain: 'social-vr-staging-52b75.firebaseapp.com',
+  databaseURL: 'https://social-vr-staging-52b75.firebaseio.com',
+  projectId: 'social-vr-staging-52b75',
+  storageBucket: 'social-vr-staging-52b75.appspot.com',
+  messagingSenderId: '415514108134',
+};
+
 export const ENV = {
-  firebase: {
-    apiKey: 'AIzaSyC5Q5Ie9To_fE2Yk8jOq1BCjIlV-9SEqQM',
-    authDomain: 'social-vr-161302.firebaseapp.com',
-    databaseURL: 'https://social-vr-161302.firebaseio.com',
-    projectId: 'social-vr-161302',
-    storageBucket: 'social-vr-161302.appspot.com',
-    messagingSenderId: '613942124685',
-  },
+  firebase,
 
   firebaseStore: {
     timestampsInSnapshots: true,
-  }
+  },
 };
